26db1ef525e44aa9cccac9e7bb7911463e6d6ad2
Jason Walsh <jwalsh@saylent.com>
2017-06-13 14:49:56 -0400

    Additional logging

---
+++ src/userscript-header.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name	JWT Logger
+// @namespace	http://wal.sh/jwt
+// @version	1.0.1
+// @description	Log JWT tokens and claims.
+// @author	Jason Walsh <j@wal.sh>
+// @match	*
+// @grant	none
+// ==/UserScript==
