c144c19a3a5a48245203183b51d35886841ef7a6
Jason Walsh <jwalsh@saylent.com>
2017-06-13 13:37:37 -0400

    Tampermonkey logging

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
-Object.keys(localStorage)
-  .forEach(e => {
-    let v = localStorage.getItem(e)
-    if (JWT_REGEX.test(v)) {
-      console.log(e, v)
-    }
-  })
+(() => {
+  const JWT_REGEX = /^[a-zA-Z0-9\-_]+?\.[a-zA-Z0-9\-_]+?\.([a-zA-Z0-9\-_]+)?$/;
+
+  const parseJwt = token => {
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace('-', '+').replace('_', '/');
+    return JSON.parse(window.atob(base64));
+  };
+
+  Object.keys(localStorage)
+    .forEach(e => {
+      const v = localStorage.getItem(e);
+      if (JWT_REGEX.test(v)) {
+        const token = v;
+        console.log(e, token);
+        alert(JSON.stringify(parseJwt(token), null, '  '));
+      }
+    });
+})();
